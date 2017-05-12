645ca1caea70e85a154425cc7e0b81740f5da6d6
timreimherr <timreimherr@gmail.com>
2017-05-12 18:04:51 -0500

    app start

---
+++ WebServiceTool/WebServiceTool/Controllers/DriverLogsController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WebServiceTool.Controllers
+{
+    public class DriverLogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private async Task<T> GetWSObject<T>(string uriActionString)
+        {
+            T returnValue = default(T);
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(@"http://localhost:53900/");
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await client.GetAsync(uriActionString);
+                    response.EnsureSuccessStatusCode();
+                    returnValue = JsonConvert.DeserializeObject<T>(((HttpResponseMessage)response).Content.ReadAsStringAsync().Result);
+                }
+
+                return returnValue;
+            }
+            catch (Exception e)
+            {
+
+                throw (e);
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+}
